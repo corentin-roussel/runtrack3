79b9d6528eb17f86024a3ff381e61e84175bd5d6
corentin-roussel <corentin.roussel@laplateforme.io>
2023-01-18 16:01:52 +0100

    rendu job01 affichage et disparition d'un phrase en jquery

--- jour03/job01/script.js
+++ jour03/job01/script.js
@@ -1,41 +1,31 @@
 $( document ).ready(function() {
 
-    const body = document.querySelector("body");
 
     const buttonShow = document.createElement("button");
-    buttonShow.setAttribute("id", "show");
-    buttonShow.textContent = ("Cliquez ici pour afficher");
-    body.appendChild(buttonShow);
+    $(buttonShow).attr('id', 'show');
+    $(buttonShow).text("Cliquez ici pour afficher");
+    $("body").append(buttonShow);
 
     const buttonHide = document.createElement("button");
-    buttonHide.setAttribute("id", "hide");
-    buttonHide.textContent = ("Cliquez ici pour cacher");
-    body.appendChild(buttonHide);
+    $(buttonHide).attr('id', 'hide');
+    $(buttonHide).text("Cliquez ici pour cacher");
+    $("body").append(buttonHide);
 
     const text = document.createElement("p");
-    text.setAttribute("id", "text");
-    text.textContent = ("Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.")
-    const idText = text.querySelector("#text");
-    body.appendChild(text);
-    text.style.display = "none";
+    $(text).attr('id', 'text');
+    $(text).text("Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.")
+    $("body").append(text);
+    $("#text").hide()
 
 
 
-    function show() {
-        if($("#show").on("click", () => {
-            return text.style.display = "block";
-        }));
-    }
+    if($("#show").on("click", () => {
+        return $("#text").show();
+    }));
 
-    function hide() {
-        if($("#hide").on("click", () => {
-            return text.style.display = "none"
-        }));
-    }
-
-    show();
-
-    hide();
+    if($("#hide").on("click", () => {
+        return $("#text").hide();
+    }));
 
 });
 
